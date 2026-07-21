de32ef9b6226c58526bf4e413b88b445cbfae3eb
y-decimal <83334510+y-decimal@users.noreply.github.com>
2026-07-21 17:08:15 +0200

    Add minimum polar bin width definition

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -196,6 +196,7 @@ typedef struct statistic_s {
 #define POLAR_BIN_COUNT 60
 #define POLAR_BIN_RANGE 1.75f // Range of how far from reference airspeed the polar bin covers as a fraction of reference airspeed, e.g. 2.0 = 200% of reference airspeed, so a reference airspeed of 100cm/s would have a polar bin range of 0cm/s to 200cm/s
 #define POLAR_BIN_RANGE_ASYMMETRY 0.3f // Asymmetry factor for polar bin range, e.g. 0.3 = 30% of range below reference airspeed and 70% above reference airspeed. In our above example we would get a range of 40cm/s to 240cm/s for a reference airspeed of 100cm/s
+#define MINIMUM_POLAR_BIN_WIDTH 50    // Minimum bin width to ensure the ability to fly within a bin 
 #define SINK_RATE_SMOOTHING_ALPHA 0.1f // Smoothing factor for sink rate averaging, 0.1 = 10% of new value, 90% of previous average
 #define POLAR_BIN_BLENDING_WIDTH 100 // Width of the blending region between polar bins in cm/s. Recommended to be set to the lowest possible precision with which an airspeed can be held
 #define POLAR_BIN_BLENDING_FACTOR 0.75f // How strong the blending should be - 0 is no blending and 1 is full blending
